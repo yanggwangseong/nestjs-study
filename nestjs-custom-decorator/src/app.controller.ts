7ebdb57518c4bf4d454b2bb672baeb2f75ca6077
YangGwangSeong <rhkdtjd_12@naver.com>
2024-11-03 23:05:59 -0800

    feat(nestjs-custom-decorator): TransformParam 커스텀 데코레이터를 만들어 string을 number로 변환 실패

--- nestjs-custom-decorator/src/app.controller.ts
+++ nestjs-custom-decorator/src/app.controller.ts
@@ -7,6 +7,7 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    const hello = 'hello';
+    return this.appService.getHello(hello);
   }
 }
--- nestjs-custom-decorator/src/app.service.ts
+++ nestjs-custom-decorator/src/app.service.ts
@@ -2,7 +2,33 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  /**
+   *
+   * Typescript 데코레이터는 런타임에서 동작하지 않아서 결국 request 요청이 들어 왔을때 string을 number로 바꿀 수 없군.
+   *
+   */
+  @TransformParam()
+  getHello(value: any): string {
+    console.log(typeof value); // number로 출력될 것
+    return `Value: ${value}`;
   }
+}
+
+function TransformParam(): MethodDecorator {
+  return (
+    target: object,
+    propertyKey: string | symbol,
+    descriptor: PropertyDescriptor,
+  ) => {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function (...args: any[]) {
+      // 첫 번째 파라미터를 숫자로 변환
+      if (typeof args[0] === 'string' && !isNaN(Number(args[0]))) {
+        args[0] = Number(args[0]);
+      }
+
+      return originalMethod.apply(this, args);
+    };
+  };
 }
